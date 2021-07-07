a0f597bb75e5f5f5761517b2e9460fd4ba730cfb
kailash2508 <86640876+kailash2508@users.noreply.github.com>
2021-07-07 19:00:15 +0530

    Update City.js

--- src/modules/City.js
+++ src/modules/City.js
@@ -43,7 +43,7 @@ const City = (props) => {
     const { updateCity, fetchWeather } = props;
     return (
         <>
-        <Logo src={"/Weather_app_using_react/icon/perfect-day.svg"} />
+        <Logo src={"/Weather_app_using_react/public/icon/perfect-day.svg"} />
         <Lable>Find weather of your city</Lable>
         <Search onSubmit={fetchWeather}>
             <input onChange={(e) => updateCity(e.target.value)} placeholder="City"/>
