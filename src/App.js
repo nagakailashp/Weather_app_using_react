fb62aca9ecc1158d799b212cac34c7bb2e2d2484
kailash2508 <86640876+kailash2508@users.noreply.github.com>
2021-07-07 18:55:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,20 +7,20 @@ import Weather from "./modules/Weather";
 const Key = "9cb3e7f53343d83260ac363d26973c4f";
 
 export const WeatherIcons = {
-  "01d": "/Weather_app_using_react/icon/sunny.svg",
-  "01n": "/Weather_app_using_react/icon/night.svg",
-  "02d": "/Weather_app_using_react/icon/day.svg",
-  "02n": "/Weather_app_using_react/icon/cloudy-night.svg",
-  "03d": "/Weather_app_using_react/icon/cloudy.svg",
-  "03n": "/Weather_app_using_react/icon/cloudy.svg",
-  "04d": "/Weather_app_using_react/icon/perfect-day.svg",
-  "04n": "/Weather_app_using_react/icon/cloudy-night.svg",
-  "09d": "/Weather_app_using_react/icon/rain.svg",
-  "09n": "/Weather_app_using_react/icon/rain-night.svg",
-  "10d": "/Weather_app_using_react/icon/rain.svg",
-  "10n": "/Weather_app_using_react/icon/rain-night.svg",
-  "11d": "/Weather_app_using_react/icon/storm.svg",
-  "11n": "/Weather_app_using_react/icon/storm.svg",
+  "01d": "public/icon/sunny.svg",
+  "01n": "public/icon/night.svg",
+  "02d": "public/icon/day.svg",
+  "02n": "public/icon/cloudy-night.svg",
+  "03d": "public/icon/cloudy.svg",
+  "03n": "public/icon/cloudy.svg",
+  "04d": "public/icon/perfect-day.svg",
+  "04n": "public/icon/cloudy-night.svg",
+  "09d": "public/icon/rain.svg",
+  "09n": "public/icon/rain-night.svg",
+  "10d": "public/icon/rain.svg",
+  "10n": "public/icon/rain-night.svg",
+  "11d": "public/icon/storm.svg",
+  "11n": "public/icon/storm.svg",
 };
 
 const Container= styled.div`
